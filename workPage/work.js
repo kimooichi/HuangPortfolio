c9b1938b29d5bc1347418ae73535114f05f1567b
kimooichi <chubbyeraser@gmail.com>
2023-09-23 17:07:10 -0400

    fixing mobile and html descriptors

--- workPage/work.js
+++ workPage/work.js
@@ -1,11 +1,11 @@
 function change1() {
-    document.getElementById("CMVCCap").innerHTML = "UI/UX DESIGN + FRONT-END";   
+    document.getElementById("CMVCCap").innerHTML = "UI DESIGN + FRONT-END";   
 }
 function revert1() {
     document.getElementById("CMVCCap").innerHTML = "2023 . CARLETON MEN'S VOLLEYBALL CLUB";   
 }
 function change2() {
-    document.getElementById("BSCap").innerHTML = "UI/UX DESIGN + FRONT-END + BRAND IDENTITY";   
+    document.getElementById("BSCap").innerHTML = "UI DESIGN + FRONT-END + BRAND IDENTITY";   
 }
 function revert2() {
     document.getElementById("BSCap").innerHTML = "2022 . BERRY SOUP";   
