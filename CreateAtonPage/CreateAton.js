fe5f0d844cce6c2c5ef51328fd8072ec4a580fb0
kimooichi <chubbyeraser@gmail.com>
2023-11-29 23:10:35 -0500

    finished createaton page

--- CreateAtonPage/CreateAton.js
+++ CreateAtonPage/CreateAton.js
@@ -1,3 +1,13 @@
 $(window).on('load', function () {
     $('#loading').hide();
   }) 
+
+  var video = document.getElementById("myVideo");
+
+  video.addEventListener("click", function() {
+      if (video.paused) {
+          video.play();
+      } else {
+          video.pause();
+      }
+  });
