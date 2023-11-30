250625e1745cf055b880e57b544b10f6a407fb6a
kimooichi <chubbyeraser@gmail.com>
2023-11-30 03:59:57 -0500

    text changes

--- morePage/more.js
+++ morePage/more.js
@@ -1,11 +1,11 @@
 function change1() {
-    document.getElementById("CMVCCap").innerHTML = "SOCIAL MEDIA CONTENT";   
+    document.getElementById("CMVCCap").innerHTML = "SOCIAL MEDIA CONTENT + DESIGN";   
 }
 function revert1() {
     document.getElementById("CMVCCap").innerHTML = "2023 . CARLETON FILM SOCIETY";   
 }
 function change2() {
-    document.getElementById("BSCap").innerHTML = "FIRST-PLACE CREATE-ATHON + ANIMATION";   
+    document.getElementById("BSCap").innerHTML = "DIGITAL ILLUSTRATION + ANIMATION";   
 }
 function revert2() {
     document.getElementById("BSCap").innerHTML = "2022 . FIRST DAY OF SCHOOL";   
