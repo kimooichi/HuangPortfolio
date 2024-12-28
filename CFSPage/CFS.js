ca0ca3a002a4bf820225f8a7d256a3d39907e10c
Kimberly Huang <118089544+kimooichi@users.noreply.github.com>
2024-12-28 15:27:09 -0500

    added js

--- CFSPage/CFS.js
+++ CFSPage/CFS.js
@@ -20,3 +20,22 @@ btn.on('click', function(e) {
 $(window).on('load', function () {
     $('#loading').hide();
   }) 
+
+  const hamburger = document.querySelector(".hamburger");
+  const navMenu = document.querySelector(".navbar");
+  
+  hamburger.addEventListener("click", mobileMenu);
+  
+  function mobileMenu() {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
+  }
+
+  const navLink = document.querySelectorAll(".navbtn a");
+
+  navLink.forEach(n => n.addEventListener("click", closeMenu));
+  
+  function closeMenu() {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+  }
